6cdbf0553123749a7007020265ca1a6d2c9add7d
ramon.valles <valles@ice.csic.es>
2022-11-12 21:36:55 +0100

    export verification to dedicated function

--- tests/build_tree/test_6_docs_test.py
+++ tests/build_tree/test_6_docs_test.py
@@ -3,8 +3,9 @@ import sys, os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 from src.mtree import Mtree
-import pprint
+from tests.build_tree.test_4_docs_test import validate_4_docs_mtree
 
+import pprint
 from hashlib import sha256
 
 
@@ -21,15 +22,50 @@ def setup_test(TEARDOWN_FIXTURE, request):
     yield {"TestHelpers" : test_helpers}
 
 
+def validate_6_docs_mtree(docs, mtree):
+    
+    # Validate subtree (docs from 0 to 3)
+    validate_4_docs_mtree(docs[:4], mtree)
+
+    # Hash document 4
+    HASH_DOC_4 = sha256( docs[4] ).hexdigest()
+    assert mtree.tree[(0,4)].getHash() == HASH_DOC_4
+
+    # Hash document 5
+    HASH_DOC_5 = sha256( DOC_5 ).hexdigest()
+    assert mtree.tree[(0,5)].getHash() == HASH_DOC_5
+
+
+    """ -- HASH LEVEL 1 --
+    """
+    NODE_1_2 = bytes(( PREFIX_NODE + HASH_DOC_4 + HASH_DOC_5 ).encode('utf-8'))
+
+    # Hash Node (1,2) which is concatenation of hashed doc0 & hashed doc1
+    HASH_NODE_1_2 = sha256( NODE_1_2 ).hexdigest()
+    assert mtree.tree[(1,2)].getHash() == HASH_NODE_1_2
+
+
+    """ -- HASH LEVEL 2 --
+    """
+    # Since Node (1,2) is an orphan child. Node (2,1) should be the same as Node (1,2)
+    HASH_NODE_2_1 = HASH_NODE_1_2
+    assert mtree.tree[(2,1)].getHash() == HASH_NODE_2_1
+
+
+    """ -- HASH LEVEL 3 (root) --
+    """
+    HASH_NODE_3_0 = verify_hash(mtree.tree[(2,0)].getHash(), HASH_NODE_2_1, mtree.tree[(3,0)].getHash() )
+
+
 def test_construct_6_docs_mtree(setup_test):
 
-    tree = Mtree()
-    tree.construct("documents")
+    mtree = Mtree()
+    mtree.construct("documents")
 
     #''' Display Tree
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(tree.tree)
-    tree.draw()
+    mtree.draw()
     #'''
 
     """ -- HASH LEVEL 0 --
@@ -41,67 +77,4 @@ def test_construct_6_docs_mtree(setup_test):
     DOC_4 = bytes((PREFIX_DOC+'hello 4').encode('utf-8'))
     DOC_5 = bytes((PREFIX_DOC+'hello 5').encode('utf-8'))
 
-    # Hash document 0
-    HASH_DOC_0 = sha256( DOC_0 ).hexdigest()
-    assert tree.tree[(0,0)].getHash() == HASH_DOC_0
-
-    # Hash document 1
-    HASH_DOC_1 = sha256( DOC_1 ).hexdigest()
-    assert tree.tree[(0,1)].getHash() == HASH_DOC_1
-
-    # Hash document 2
-    HASH_DOC_2 = sha256( DOC_2 ).hexdigest()
-    assert tree.tree[(0,2)].getHash() == HASH_DOC_2
-
-    # Hash document 3
-    HASH_DOC_3 = sha256( DOC_3 ).hexdigest()
-    assert tree.tree[(0,3)].getHash() == HASH_DOC_3
-
-    # Hash document 4
-    HASH_DOC_4 = sha256( DOC_4 ).hexdigest()
-    assert tree.tree[(0,4)].getHash() == HASH_DOC_4
-
-    # Hash document 5
-    HASH_DOC_5 = sha256( DOC_5 ).hexdigest()
-    assert tree.tree[(0,5)].getHash() == HASH_DOC_5
-
-    """ -- HASH LEVEL 1 --
-    """
-    NODE_1_0 = bytes(( PREFIX_NODE + HASH_DOC_0 + HASH_DOC_1 ).encode('utf-8'))
-    NODE_1_1 = bytes(( PREFIX_NODE + HASH_DOC_2 + HASH_DOC_3 ).encode('utf-8'))
-    NODE_1_2 = bytes(( PREFIX_NODE + HASH_DOC_4 + HASH_DOC_5 ).encode('utf-8'))
-
-    # Hash Node (1,0) which is concatenation of hashed doc0 & hashed doc1
-    HASH_NODE_1_0 = sha256( NODE_1_0 ).hexdigest()
-    assert tree.tree[(1,0)].getHash() == HASH_NODE_1_0
-
-    # Hash Node (1,1) which is concatenation of hashed doc2 & hashed doc3
-    HASH_NODE_1_1 = sha256( NODE_1_1 ).hexdigest()
-    assert tree.tree[(1,1)].getHash() == HASH_NODE_1_1
-
-    HASH_NODE_1_2 = sha256( NODE_1_2 ).hexdigest()
-    # Since document 4 is an orphan child. Node (1,2) should be the same as Node (0,4)
-    assert tree.tree[(1,2)].getHash() == HASH_NODE_1_2
-
-
-    """ -- HASH LEVEL 2 --
-    """
-    NODE_2_0 = bytes(( PREFIX_NODE + HASH_NODE_1_0 + HASH_NODE_1_1 ).encode('utf-8'))
-    NODE_2_1 = HASH_NODE_1_2
-
-    # Hash Node (2,0) which is concatenation of hashed Node (1,0) & Node (1,1)
-    HASH_NODE_2_0 = sha256( NODE_2_0 ).hexdigest()
-    assert tree.tree[(2,0)].getHash() == HASH_NODE_2_0
-
-    HASH_NODE_2_1 = NODE_2_1
-    # Since Node (1,3) 4 is an orphan child. Node (2,1) should be the same as Node (1,2)
-    assert tree.tree[(2,1)].getHash() == HASH_NODE_2_1
-
-
-    """ -- HASH LEVEL 3 (root) --
-    """
-    NODE_3_0 = bytes(( PREFIX_NODE + HASH_NODE_2_0 + HASH_NODE_2_1 ).encode('utf-8'))
-
-    # Hash Node (2,0) which is concatenation of hashed Node (1,0) & Node (1,1)
-    HASH_NODE_3_0 = sha256( NODE_3_0 ).hexdigest()
-    assert tree.tree[(3,0)].getHash() == HASH_NODE_3_0
+    validate_6_docs_mtree([DOC_0,DOC_1,DOC_2,DOC_3,DOC_4,DOC_5], mtree)
